447c9a32dcc04e30b66c0050e3a33e857e85a8d1
stormeye2000 <info@stormeye.co.uk>
2023-07-07 10:37:05 +0100

    get latest block tests in place and working

--- go.mod
+++ go.mod
@@ -3,13 +3,20 @@ module github.com/stormeye2000/cspr-sdk-standard-tests-go
 go 1.20
 
 require (
+	github.com/acarl005/stripansi v0.0.0-20180116102854-5a71ef0e047d
 	github.com/cucumber/godog v0.12.6
 	github.com/make-software/casper-go-sdk v1.1.4
 	gopkg.in/yaml.v2 v2.4.0
 )
 
 require (
-	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
+	github.com/btcsuite/btcd v0.22.3 // indirect
 	github.com/cucumber/gherkin-go/v19 v19.0.3 // indirect
 	github.com/cucumber/messages-go/v16 v16.0.1 // indirect
 	github.com/gofrs/uuid v4.2.0+incompatible // indirect
@@ -18,6 +25,7 @@ require (
 	github.com/hashicorp/golang-lru v0.5.4 // indirect
 	github.com/shopspring/decimal v1.3.1 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/stretchr/testify v1.8.2
 	golang.org/x/crypto v0.9.0 // indirect
 	golang.org/x/sys v0.8.0 // indirect
 )
---
+++ tests/utils/utils.go
@@ -0,0 +1,79 @@
+package utils
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/acarl005/stripansi"
+	"github.com/make-software/casper-go-sdk/casper"
+	"github.com/stretchr/testify/assert"
+	yml "gopkg.in/yaml.v2"
+	"log"
+	"net/http"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"runtime"
+)
+
+var (
+	_, b, _, _ = runtime.Caller(0)
+	root       = filepath.Join(filepath.Dir(b), "../..")
+	config     map[string]interface{}
+)
+
+func Sdk() casper.RPCClient {
+
+	host := fmt.Sprintf("%v", config["host-name"])
+	port := fmt.Sprintf("%v", config["port-rcp"])
+
+	var handler = casper.NewRPCHandler("http://"+host+":"+port+"/rpc", http.DefaultClient)
+	return casper.NewRPCClient(handler)
+
+}
+
+func ReadConfig() {
+	f, err := os.ReadFile(root + "/config.yml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = yml.Unmarshal(f, &config)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func GetNctlLatestBlock() (error, casper.Block) {
+
+	docker := fmt.Sprintf("%v", config["docker-name"])
+	block := casper.Block{}
+
+	res, err := exec.Command("/bin/sh", "-c",
+		"docker exec  -t "+docker+" /bin/bash -c 'source casper-node/utils/nctl/sh/views/view_chain_block.sh'",
+		"| sed -e \"s/\\x1b\\[.\\{1,5\\}m//g\"").Output()
+	if err != nil {
+		fmt.Println("could not run command: ", err)
+	}
+
+	err = json.Unmarshal([]byte(stripansi.Strip(string(res))), &block)
+	if err != nil {
+		fmt.Println("could not unmarshal: ", err)
+	}
+
+	return err, block
+}
+
+func AssertExpectedAndActual(a expectedAndActualAssertion, expected, actual interface{}) error {
+	var t asserter
+	a(&t, expected, actual)
+	return t.err
+}
+
+type asserter struct {
+	err error
+}
+
+func (a *asserter) Errorf(format string, args ...interface{}) {
+	a.err = fmt.Errorf(format, args...)
+}
+
+type expectedAndActualAssertion func(t assert.TestingT, expected, actual interface{}, msgAndArgs ...interface{}) bool
