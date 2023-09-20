80109f484fc9aa7e410c749d54cd8052caeb97e5
meywood <105049338+meywood@users.noreply.github.com>
2023-09-20 17:19:43 +0100

    issues/39 - removed redundant Result method from utils

--- tests/steps/blocks_test.go
+++ tests/steps/blocks_test.go
@@ -59,27 +59,27 @@ func InitializeBlocksScenario(ctx *godog.ScenarioContext) {
 		err := utils.AssertExpectedAndActual(
 			assert.Equal, contextMap.blockDataSdk.Block.Body, contextMap.blockDataNode.Body,
 		)
-		return utils.Result(err)
+		return err
 	})
 
 	ctx.Step(`^the hash of the returned block is equal to the hash of the returned test node block$`, func() error {
 		err := utils.AssertExpectedAndActual(
 			assert.Equal, contextMap.blockDataSdk.Block.Hash, contextMap.blockDataNode.Hash,
 		)
-		return utils.Result(err)
+		return err
 	})
 
 	ctx.Step(`^the header of the returned block is equal to the header of the returned test node block$`, func() error {
 		err := utils.AssertExpectedAndActual(
 			assert.Equal, contextMap.blockDataSdk.Block.Header, contextMap.blockDataNode.Header,
 		)
-		return utils.Result(err)
+		return err
 	})
 
 	ctx.Step(`^the proofs of the returned block are equal to the proofs of the returned test node block$`, func() error {
 		err := utils.AssertExpectedAndActual(
 			assert.Equal, contextMap.blockDataSdk.Block.Proofs, contextMap.blockDataNode.Proofs,
 		)
-		return utils.Result(err)
+		return err
 	})
 }
